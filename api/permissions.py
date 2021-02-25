0fad2e6515671d1e5262e56680ac6c454ac19ca2
Ivan Sushkov <68387479+ionesu@users.noreply.github.com>
2021-02-25 21:28:33 +0200

    Update permissions.py
    -

--- api/permissions.py
+++ api/permissions.py
@@ -4,5 +4,5 @@ from rest_framework import permissions
 class IsOwnerOrReadOnly(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
-        if obj.author == request.user and request.method in permissions.SAFE_METHODS:
-            return True
+        if request.method in permissions.SAFE_METHODS:
+            return obj.author == request.user
