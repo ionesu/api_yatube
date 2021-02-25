8cc57a4d0f62fadb0d3f5ece1705d72a89148ff5
Ivan Sushkov <ivan.sushkov91@gmail.com>
2021-02-25 21:51:35 +0200

    -

--- api/permissions.py
+++ api/permissions.py
@@ -4,5 +4,5 @@ from rest_framework import permissions
 class IsOwnerOrReadOnly(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return obj.author == request.user
+        if obj.author == request.user:
+            return True
